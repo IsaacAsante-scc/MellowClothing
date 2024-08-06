897e7d338fcc13d3674a59671148127e960bc7c7
Isaac Asante <isaacka44@gmail.com>
2024-08-06 03:58:52 -0400

    Initial commit

---
+++ app/src/main/java/com/example/mellowclothing/BaseActivity.java
@@ -0,0 +1,18 @@
+package com.example.mellowclothing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+public class BaseActivity extends AppCompatActivity {
+    FirebaseDatabase database;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        database = FirebaseDatabase.getInstance();sasdsada
+    }
+}
